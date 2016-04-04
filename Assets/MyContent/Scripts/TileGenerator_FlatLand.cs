cb4d8a09b61df270d042b0399c19244e55d23c5e
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-04-04 22:18:43 +0200

    Optimize tile generator

--- Assets/MyContent/Scripts/TileGenerator_FlatLand.cs
+++ Assets/MyContent/Scripts/TileGenerator_FlatLand.cs
@@ -8,64 +8,72 @@ public abstract class TileGenerator : MonoBehaviour {
 	public GameObject tile;
 
 	const int m_tileRadius = 2;
-	int m_tileWidth;
-	Vector3 m_playerStartPos;
+	const int m_tileDiameter = m_tileRadius * 2;
+	const int m_tileCount = m_tileDiameter * m_tileDiameter;
+	const int m_tileWidth = 10;
 
-	List<GameObject> m_tilePool = new List<GameObject>();
+	int m_currentTileX;
+	int m_currentTileZ;
+
+	List<GameObject> m_tileList = new List<GameObject>();
+	int m_topIndex = m_tileDiameter - 1;
+	int m_leftIndex = 0;
 
 	// Use this for initialization
 	void Start () {
-		for (int t = 0; t < m_tileRadius * m_tileRadius * 4; ++t)
-			m_tilePool.Add((GameObject)Instantiate(tile, Vector3.zero, Quaternion.identity));
-				
-		float w = m_tilePool[0].GetComponent<Renderer>().bounds.size.x;
-		m_tileWidth = (int)w;
-		Debug.AssertFormat(m_tileWidth == w, "Tile width needs to be integer");
-		m_playerStartPos = player.transform.position;
+		Vector3 playerPos = player.transform.position;
+		m_currentTileX = Mathf.FloorToInt(playerPos.x / m_tileWidth);
+		m_currentTileZ = Mathf.FloorToInt(playerPos.z / m_tileWidth);
 
-		int tileIndex = 0;
-		for (int xTile = -m_tileRadius; xTile < m_tileRadius; ++xTile) {
-			for (int zTile = -m_tileRadius; zTile < m_tileRadius; ++zTile) {
-				float tilePosX = (int)m_playerStartPos.x + (xTile * m_tileWidth) + (m_tileWidth / 2);
-				float tilePosZ = (int)m_playerStartPos.z + (zTile * m_tileWidth) + (m_tileWidth / 2);
-				GameObject tileObject = m_tilePool[tileIndex];
-				tileObject.transform.position = new Vector3(tilePosX, 0, tilePosZ);
-				updateTile(tileObject);
-				tileIndex++;
-			}
+		for (int t = 0; t < m_tileCount; ++t) {
+			GameObject tileObject = (GameObject)Instantiate(tile, Vector3.zero, Quaternion.identity);
+			m_tileList.Add(tileObject);
+			int x = (t % m_tileDiameter) - m_tileRadius;
+			int z = (int)(t / m_tileDiameter) - m_tileRadius;
+			float tilePosX = Mathf.FloorToInt(playerPos.x) + (x * m_tileWidth) + m_tileWidth;
+			float tilePosZ = Mathf.FloorToInt(playerPos.z) + (z * m_tileWidth) + m_tileWidth;
+			tileObject.transform.position = new Vector3(tilePosX, 0, tilePosZ);
 		}
+				
+		float w = m_tileList[0].GetComponent<Renderer>().bounds.size.x;
+		Debug.AssertFormat(m_tileWidth == w, "Game object tile width does not match const m_tileWidth");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 currentPlayerPos = player.transform.position;
-		int xMove = (int)(currentPlayerPos.x - m_playerStartPos.x);
-		int zMove = (int)(currentPlayerPos.z - m_playerStartPos.z);
-		if (Mathf.Abs(xMove) < m_tileWidth && Mathf.Abs(zMove) < m_tileWidth)
+		Vector3 playerPos = player.transform.position;
+		int tileX = Mathf.FloorToInt(playerPos.x / m_tileWidth);
+		int tileZ = Mathf.FloorToInt(playerPos.z / m_tileWidth);
+		if (tileX == m_currentTileX && tileZ == m_currentTileZ)
 			return;
 
+		int tileMoveX = tileX - m_currentTileX;
+		int tileMoveZ = tileZ - m_currentTileZ;
 
-		/*
-		Finn nytt center (player player pos % m_tileRadius)
-			center + m_tileWidth => top
+		m_topIndex = (m_topIndex + tileMoveZ) % m_tileDiameter;
+		m_topIndex += m_topIndex < 0 ? m_tileDiameter : 0;
+		m_leftIndex = 0;//(m_leftIndex + tileMoveX) % m_tileDiameter;
 
-			rad som kan frigjøres: (center - gammelt center) > 0 ? center - m_tileRadius : center + m_tileRadius;
-*/
+		print(m_currentTileZ + ", " + tileZ + ", " + tileMoveZ + ", " + playerPos.z);
 
-		print("update needed");
-		m_playerStartPos = currentPlayerPos;
+		// todo: handle if tileMoveZ > 1
+		int bottomIndex = (m_topIndex + 1) % m_tileDiameter;
+		int verticalIndex = tileMoveZ > 0 ? m_topIndex : bottomIndex;
 
-		int tileIndex = 0;
-		for (int xTile = -m_tileRadius; xTile < m_tileRadius; ++xTile) {
-			for (int zTile = -m_tileRadius; zTile < m_tileRadius; ++zTile) {
-				int tilePosX = (int)m_playerStartPos.x + (xTile * m_tileWidth) + (m_tileWidth / 2);
-				int tilePosZ = (int)m_playerStartPos.z + (zTile * m_tileWidth) + (m_tileWidth / 2);
-				GameObject tileObject = m_tilePool[tileIndex];
-				tileObject.transform.position = new Vector3(tilePosX, 0, tilePosZ);
-				updateTile(tileObject);
-				tileIndex++;
-			}
+		for (int x = 0; x < m_tileDiameter; ++x) {
+			// Figure out which tile should be reused
+			int tileLeftIndex = (m_leftIndex + x) % m_tileDiameter;
+			int tileListIndex = (verticalIndex * m_tileDiameter) + tileLeftIndex;
+			GameObject tileObject = m_tileList[tileListIndex];
+
+			// Calculate where the tile should be moved 
+			int zOffset = tileMoveZ > 0 ? m_tileRadius - 1 : -m_tileRadius;
+			float tilePosZ = ((tileZ + zOffset) * m_tileWidth) + m_tileWidth;
+			tileObject.transform.position = new Vector3(tileObject.transform.position.x, 0, tilePosZ);
 		}
+
+		m_currentTileX = tileX;
+		m_currentTileZ = tileZ;
 	}
 
 	public abstract void updateTile(GameObject tile);
@@ -75,6 +83,5 @@ public class TileGenerator_FlatLand : TileGenerator {
 
 	public override void updateTile(GameObject tile)
 	{
-		print("update tile" + tile.transform.position.x);
 	}
 }
