86edb2003d254c651493e0f5a4645948f374f26b
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-04-05 08:46:55 +0200

    add everything

---
+++ Assets/createCubes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createCubes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Starting to create cubes");
+		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		cube.transform.position = new Vector3 (0, 0, -7);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
