0e487a51446d2a535ec472bc4d9555eb03f5e144
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-04-19 19:41:48 +0200

    Add missing files

---
+++ Assets/MyContent/Scripts/SatteliteCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatteliteCamera : MonoBehaviour {
+	public GameObject objectToTrack;
+	public float orbitHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = objectToTrack.transform.position;
+		pos.y = orbitHeight;
+		transform.position = pos;
+		transform.LookAt(objectToTrack.transform);
+	}
+}
