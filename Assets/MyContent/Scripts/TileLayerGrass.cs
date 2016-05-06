a081623ff0fdc98444492954b838b51190ef2baa
Richard Moe Gustavsen <richard.gustavsen@theqtcompany.com>
2016-05-06 15:28:22 +0200

    Remove LandscapeTile

--- Assets/MyContent/Scripts/TileLayerGrass.cs
+++ Assets/MyContent/Scripts/TileLayerGrass.cs
@@ -4,30 +4,35 @@ using System.Collections.Generic;
 
 public class TileLayerGrass : ITileLayer 
 {
+	GameObject m_prefab;
 	GameObject m_itemsParent;
 	GameObject[] m_items;
 	const int max_items = 100;
 
-	public TileLayerGrass(string name, GameObject tilePrefab, Transform parentTransform)
+	public TileLayerGrass(string name, GameObject prefab, Transform parentTransform)
 	{
+		m_prefab = prefab;
 		m_itemsParent = new GameObject(name);
 		m_itemsParent.transform.SetParent(parentTransform);
+		m_items = new GameObject[max_items];
 	}
 
 	public void initTiles(TileDescription[] tilesToInit)
 	{
-		for (int i = 0; i < tilesToInit.Length; ++i) {
-//			ITile tileScript = (ITile)getGameObject(tilesToInit[i].matrixCoord).GetComponent<ITile>();
-//			Debug.AssertFormat(tileScript != null, "TileTerrainLayer: tilePrefab needs to have a script attached that implements ITile");
-//			tileScript.initTile(tilesToInit[i], getGameObject(tilesToInit[i].matrixCoord));
-		}
+//		for (int i = 0; i < tilesToInit.Length; ++i) {
+//			GameObject grassObject = (GameObject)GameObject.Instantiate(m_prefab, Vector3.zero, Quaternion.identity);
+//			m_items[i] = grassObject;
+//			ITile tileScript = (ITile)grassObject.GetComponent<ITile>();
+//			Debug.AssertFormat(tileScript != null, this.GetType().Name + ": prefab needs to have a script attached that implements ITile");
+//			tileScript.initTile(tilesToInit[i], grassObject);
+//		}
 	}
 
 	public void moveTiles(TileDescription[] tilesToMove)
 	{
-		for (int i = 0; i < tilesToMove.Length; ++i) {
+//		for (int i = 0; i < tilesToMove.Length; ++i) {
 //			GameObject gameObject = getGameObject(tilesToMove[i].matrixCoord);
 //			gameObject.GetComponent<ITile>().moveTile(tilesToMove[i], gameObject);
-		}
+//		}
 	}
 }
--- Assets/MyContent/Scripts/TileLayerTerrain.cs
+++ Assets/MyContent/Scripts/TileLayerTerrain.cs
@@ -5,9 +5,15 @@ using System.Collections.Generic;
 public class TileLayerTerrain : ITileTerrainLayer
 {
 	GameObject[,] m_tileMatrix;
+	public float[,] m_heightArray;
 
-	public TileLayerTerrain(string name, GameObject tilePrefab, Transform parentTransform)
+	public TileLayerTerrain(string name, GameObject prefab, Transform parentTransform)
 	{
+		Terrain terrain = prefab.GetComponent<Terrain>();
+		Debug.AssertFormat(terrain != null, this.GetType().Name + ": prefab needs to have a Terrain component");
+		int res = terrain.terrainData.heightmapResolution;
+		m_heightArray = new float[res, res];
+
 		int count = LandscapeConstructor.m_instance.rows;
 		m_tileMatrix = new GameObject[count, count];
 		Transform childRoot = parentTransform.Find(name);
@@ -15,13 +21,13 @@ public class TileLayerTerrain : ITileTerrainLayer
 		// If a child with name already exists, adopt its
 		// children instead of creating new ones.
 		if (childRoot == null)
-			constructNewGameobjects(name, tilePrefab, parentTransform);
+			constructNewGameobjects(name, prefab, parentTransform);
 		else
 			adoptChildren(childRoot);
 
 	}
 
-	public void constructNewGameobjects(string name, GameObject tilePrefab, Transform parentTransform)
+	public void constructNewGameobjects(string name, GameObject prefab, Transform parentTransform)
 	{
 		Transform tilesParent = new GameObject(name).transform;
 		tilesParent.SetParent(parentTransform);
@@ -29,7 +35,7 @@ public class TileLayerTerrain : ITileTerrainLayer
 
 		for (int z = 0; z < count; ++z) {
 			for (int x = 0; x < count; ++x) {
-				m_tileMatrix[x, z] = (GameObject)GameObject.Instantiate(tilePrefab, Vector3.zero, Quaternion.identity);
+				m_tileMatrix[x, z] = (GameObject)GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
 				m_tileMatrix[x, z].transform.SetParent(tilesParent);
 			}
 		}
@@ -46,45 +52,72 @@ public class TileLayerTerrain : ITileTerrainLayer
 		}
 	}
 
-	GameObject getGameObject(Vector2 matrixPos)
-	{
-		return (int)matrixPos.x == -1 ? null : m_tileMatrix[(int)matrixPos.x, (int)matrixPos.y];
-	}
-
-	Terrain getTerrain(Vector2 matrixPos)
-	{
-		return (int)matrixPos.x == -1 ? null : m_tileMatrix[(int)matrixPos.x, (int)matrixPos.y].GetComponent<Terrain>();
-	}
-
 	public void initTiles(TileDescription[] tilesToInit)
 	{
 		for (int i = 0; i < tilesToInit.Length; ++i) {
-			ITile tileScript = (ITile)getGameObject(tilesToInit[i].matrixCoord).GetComponent<ITile>();
-			Debug.AssertFormat(tileScript != null, "TileTerrainLayer: tilePrefab needs to have a script attached that implements ITile");
-			tileScript.initTile(tilesToInit[i], getGameObject(tilesToInit[i].matrixCoord));
+			TileDescription desc = tilesToInit[i];
+			GameObject tileObject = m_tileMatrix[(int)desc.matrixCoord.x, (int)desc.matrixCoord.y];
+			Terrain terrain = tileObject.GetComponent<Terrain>();
+			TerrainData tdata = terrain.terrainData;
+
+			if (desc.gridCoord.x == 0 && desc.gridCoord.y == 0) {
+				Vector3 scale = tdata.heightmapScale;
+				float w = tdata.size.x;
+				float l = tdata.size.z;
+				Debug.AssertFormat(scale.y == LandscapeConstructor.m_instance.landscapeHeightLargeScale, "LandscapeTile: Landscape height needs to match global height function");
+				Debug.AssertFormat(w == l && w == (float)LandscapeConstructor.m_instance.tileWidth, "LandscapeTile: landscape size needs to be the same as in tileWidth");
+			}
+
+			terrain.terrainData = LandscapeTools.Clone(tdata);
+			tileObject.GetComponent<TerrainCollider>().terrainData = terrain.terrainData;
 		}
+
+		moveTiles(tilesToInit);
 	}
 
 	public void moveTiles(TileDescription[] tilesToMove)
 	{
 		for (int i = 0; i < tilesToMove.Length; ++i) {
-			GameObject gameObject = getGameObject(tilesToMove[i].matrixCoord);
-			gameObject.GetComponent<ITile>().moveTile(tilesToMove[i], gameObject);
+			TileDescription desc = tilesToMove[i];
+			GameObject tileObject = m_tileMatrix[(int)desc.matrixCoord.x, (int)desc.matrixCoord.y];
+			Terrain terrain = tileObject.GetComponent<Terrain>();
+			TerrainData tdata = terrain.terrainData;
+
+			tileObject.transform.position = desc.worldPos;
+
+			int res = tdata.heightmapResolution;
+			Vector3 scale = tdata.heightmapScale;
+
+			for (int x = 0; x < res; ++x) {
+				for (int z = 0; z < res; ++z) {
+					float height = LandscapeConstructor.getGroundHeight(desc.worldPos.x + (x * scale.x), desc.worldPos.z + (z * scale.z));
+					m_heightArray[z, x] = height / scale.y;
+				}
+			}
+
+			tdata.SetHeights(0, 0, m_heightArray);
 		}
 	}
 
 	public void updateTileNeighbours(TileDescription[] tilesWithNewNeighbours)
 	{
 		for (int i = 0; i < tilesWithNewNeighbours.Length; ++i) {
-			Terrain t = getTerrain(tilesWithNewNeighbours[i].matrixCoord);
+			TileDescription desc = tilesWithNewNeighbours[i];
+			GameObject tileObject = m_tileMatrix[(int)desc.matrixCoord.x, (int)desc.matrixCoord.y];
+			Terrain terrain = tileObject.GetComponent<Terrain>();
 			TileNeighbours tn = tilesWithNewNeighbours[i].neighbours;
 
-			Terrain top = getTerrain(tn.top);
-			Terrain bottom = getTerrain(tn.bottom);
-			Terrain left = getTerrain(tn.left);
-			Terrain right = getTerrain(tn.right);
+			Terrain top = getTerrainSafe(tn.top);
+			Terrain bottom = getTerrainSafe(tn.bottom);
+			Terrain left = getTerrainSafe(tn.left);
+			Terrain right = getTerrainSafe(tn.right);
 
-			t.SetNeighbors(left, top, right, bottom);
+			terrain.SetNeighbors(left, top, right, bottom);
 		}
+	}
+
+	Terrain getTerrainSafe(Vector2 matrixPos)
+	{
+		return (int)matrixPos.x == -1 ? null : m_tileMatrix[(int)matrixPos.x, (int)matrixPos.y].GetComponent<Terrain>();
 	}
 }
