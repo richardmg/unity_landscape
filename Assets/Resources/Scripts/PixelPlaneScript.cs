4692879064cd89c452c8b0c9a0587d11f1126bbc
Richard Moe Gustavsen <richard.gustavsen@theqtcompany.com>
2016-05-27 13:53:34 +0200

    Rename MyAssets to Resources

--- Assets/Resources/Scripts/PixelPlaneScript.cs
+++ Assets/Resources/Scripts/PixelPlaneScript.cs
@@ -3,24 +3,22 @@ using System.Collections;
 
 public class PixelPlaneScript : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
-		Mesh mesh = createMeshCube();	
+		Mesh mesh = createMeshCube(10, 10, 10);	
 		MeshFilter meshFilter = (MeshFilter)gameObject.AddComponent<MeshFilter>();
 		meshFilter.mesh = mesh;
 
 		MeshRenderer meshRenderer = (MeshRenderer)gameObject.AddComponent<MeshRenderer>();
-//		meshRenderer.material.shader = Shader.Find("Custom/CutoffM");
-		meshRenderer.material = (Material)Resources.Load("Custom/CutoffM");
+		meshRenderer.material = (Material)Resources.Load("Materials/CutoffM");
 	}
 
-	Mesh createMeshCube()
+	Mesh createMeshCube(float width, float height, float depth)
 	{
 		Vector3[] v = new Vector3[4];
-		v[0].x = -10; v[0].y = 0;  v[0].z = 0;
+		v[0].x = 0; v[0].y = 0;  v[0].z = 0;
 		v[1].x = 10;  v[1].y = 0;  v[1].z = 0;
-		v[2].x = -10; v[2].y = 50; v[2].z = 0;
-		v[3].x = 10;  v[3].y = 50; v[3].z = 0;
+		v[2].x = 0; v[2].y = 10; v[2].z = 0;
+		v[3].x = 10;  v[3].y = 10; v[3].z = 0;
 
 		Vector2[] uv = new Vector2[4];
 		uv[0].x = 0; uv[0].y = 0;
@@ -37,7 +35,6 @@ public class PixelPlaneScript : MonoBehaviour {
 		tri[5] = 2;
 
 		Mesh mesh = new Mesh();
-//		mesh.Clear();
 		mesh.vertices = v;
 		mesh.uv = uv;
 		mesh.triangles = tri;
