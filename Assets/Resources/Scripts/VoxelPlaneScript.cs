c3d218d184043bac789f380d8680671ae3786fd5
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-05-27 19:47:45 +0200

    Add twosided voxels

--- Assets/Resources/Scripts/VoxelPlaneScript.cs
+++ Assets/Resources/Scripts/VoxelPlaneScript.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class VoxelPlaneScript : MonoBehaviour {
 
 	void Start () {
-		Mesh mesh0 = createVoxelMesh(1, 1, 1, 1);	
-		Mesh mesh1 = createVoxelMesh(1, 1, 1, 1);	
+		Mesh mesh0 = createVoxelMesh(1, 10, 10, 1);	
+		Mesh mesh1 = createVoxelMesh(1, 10, 10, 1);	
 
 		Matrix4x4 transform0 = new Matrix4x4();
 		transform0.SetTRS(new Vector3(-10, 10, 0), Quaternion.identity, new Vector3(1, 1, 1));
@@ -28,29 +28,55 @@ public class VoxelPlaneScript : MonoBehaviour {
 
 		MeshRenderer meshRenderer = (MeshRenderer)gameObject.AddComponent<MeshRenderer>();
 		meshRenderer.material = (Material)Resources.Load("Materials/CutoffM");
+		// CHANGE MATERIAL TO NOT BE TWO-SIDED
 	}
 
 	Mesh createVoxelMesh(int voxelCount, float voxelWidth, float voxelHeight, float voxelDepth)
 	{
-		Vector3[] v = new Vector3[4];
-		v[0].x = 0; v[0].y = 0;  v[0].z = 0;
-		v[1].x = 10;  v[1].y = 0;  v[1].z = 0;
-		v[2].x = 0; v[2].y = 10; v[2].z = 0;
-		v[3].x = 10;  v[3].y = 10; v[3].z = 0;
+		float w = voxelCount * voxelWidth;
+		float h = voxelHeight;
 
-		Vector2[] uv = new Vector2[4];
+		Vector3[] v = new Vector3[8];
+		Vector2[] uv = new Vector2[8];
+		int[] tri = new int[12];
+
+		// Front vertices
+		v[0].x = 0; v[0].y = 0; v[0].z = 0;
+		v[1].x = w; v[1].y = 0; v[1].z = 0;
+		v[2].x = 0; v[2].y = h; v[2].z = 0;
+		v[3].x = w; v[3].y = h; v[3].z = 0;
+
+		// Back vertices
+		for (int i = 4; i < 8; ++i) {
+			v[i] = v[i - 4];
+			v[i].z = voxelDepth;
+		}
+
+		// Front texture coords
 		uv[0].x = 0; uv[0].y = 0;
 		uv[1].x = 2; uv[1].y = 0;
 		uv[2].x = 0; uv[2].y = 1;
 		uv[3].x = 2; uv[3].y = 1;
 
-		int[] tri = new int[6];
+		// Back texture coords
+		for (int i = 4; i < 8; ++i)
+			uv[i] = uv[i - 4];
+
+		// Front triangles
 		tri[0] = 0;
 		tri[1] = 1;
 		tri[2] = 2;
 		tri[3] = 1;
 		tri[4] = 3;
 		tri[5] = 2;
+
+		// Back triangles
+		tri[6] = 5;
+		tri[7] = 4;
+		tri[8] = 7;
+		tri[9] = 4;
+		tri[10] = 6;
+		tri[11] = 7;
 
 		Mesh mesh = new Mesh();
 		mesh.vertices = v;
