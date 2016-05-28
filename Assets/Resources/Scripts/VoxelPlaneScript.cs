8d387431c9175896ced939e70bfdab1ac5985155
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-05-29 00:26:12 +0200

    VoxelPlane: create cube using 24 vertices

--- Assets/Resources/Scripts/VoxelPlaneScript.cs
+++ Assets/Resources/Scripts/VoxelPlaneScript.cs
@@ -16,7 +16,6 @@ public class VoxelPlaneScript : MonoBehaviour {
 	void Start () {
 		cols = texture.width;
 		rows = texture.height;
-		print("c:" + cols + ", r:" + rows);
 
 		List<CombineInstance> ciList = new List<CombineInstance>();
 
@@ -88,13 +87,14 @@ public class VoxelPlaneScript : MonoBehaviour {
 	{
 		float w = (voxelX2 - voxelX1) * voxelWidth;
 		float h = voxelHeight;
+		float z = voxelDepth;
 		float sx = (1.0f / cols) * voxelX1;
 		float ex = (1.0f / cols) * voxelX2;
 		float sy = (1.0f / rows) * voxelY;
 		float ey = (1.0f / rows) * (voxelY + 1);
 
-		Vector3[] v = new Vector3[8];
-		Vector2[] uv = new Vector2[8];
+		Vector3[] v = new Vector3[24];
+		Vector2[] uv = new Vector2[24];
 		int[] tri = new int[36];
 
 		// Front vertices
@@ -104,10 +104,34 @@ public class VoxelPlaneScript : MonoBehaviour {
 		v[3].x = w; v[3].y = h; v[3].z = 0;
 
 		// Back vertices
-		for (int i = 4; i < 8; ++i) {
-			v[i] = v[i - 4];
-			v[i].z = voxelDepth;
-		}
+		v[4].x = 0; v[4].y = 0; v[4].z = z;
+		v[5].x = 0; v[5].y = h; v[5].z = z;
+		v[6].x = w; v[6].y = 0; v[6].z = z;
+		v[7].x = w; v[7].y = h; v[7].z = z;
+
+		// Top vertices
+		v[ 8].x = 0; v[ 8].y = h; v[ 8].z = 0;
+		v[ 9].x = 0; v[ 9].y = h; v[ 9].z = z;
+		v[10].x = w; v[10].y = h; v[10].z = 0;
+		v[11].x = w; v[11].y = h; v[11].z = z;
+
+		// Bottom vertices
+		v[12].x = 0; v[12].y = 0; v[12].z = 0;
+		v[13].x = 0; v[13].y = 0; v[13].z = z;
+		v[14].x = w; v[14].y = 0; v[14].z = 0;
+		v[15].x = w; v[15].y = 0; v[15].z = z;
+
+		// Left vertices
+		v[16].x = 0; v[16].y = 0; v[16].z = z;
+		v[17].x = 0; v[17].y = h; v[17].z = z;
+		v[18].x = 0; v[18].y = 0; v[18].z = 0;
+		v[19].x = 0; v[19].y = h; v[19].z = 0;
+
+		// Right vertices
+		v[20].x = w; v[20].y = 0; v[20].z = z;
+		v[21].x = w; v[21].y = h; v[21].z = z;
+		v[22].x = w; v[22].y = 0; v[22].z = 0;
+		v[23].x = w; v[23].y = h; v[23].z = 0;
 
 		// Front texture coords
 		uv[0].x = sx; uv[0].y = sy;
@@ -116,8 +140,34 @@ public class VoxelPlaneScript : MonoBehaviour {
 		uv[3].x = ex; uv[3].y = ey;
 
 		// Back texture coords
-		for (int i = 4; i < 8; ++i)
-			uv[i] = uv[i - 4];
+		uv[4].x = sx; uv[4].y = sy;
+		uv[5].x = sx; uv[5].y = ey;
+		uv[6].x = ex; uv[6].y = sy;
+		uv[7].x = ex; uv[7].y = ey;
+
+		// Top texture coords
+		uv[ 8].x = sx; uv[ 8].y = ey;
+		uv[ 9].x = sx; uv[ 9].y = ey;
+		uv[10].x = ex; uv[10].y = ey;
+		uv[11].x = ex; uv[11].y = ey;
+
+		// Bottom texture coords
+		uv[12].x = sx; uv[12].y = sy;
+		uv[13].x = sx; uv[13].y = sy;
+		uv[14].x = ex; uv[14].y = sy;
+		uv[15].x = ex; uv[15].y = sy;
+
+		// Left texture coords
+		uv[16].x = sx; uv[16].y = sy;
+		uv[17].x = sx; uv[17].y = ey;
+		uv[18].x = sx; uv[18].y = sy;
+		uv[19].x = sx; uv[19].y = ey;
+
+		// Right texture coords
+		uv[20].x = ex; uv[20].y = sy;
+		uv[21].x = ex; uv[21].y = ey;
+		uv[22].x = ex; uv[22].y = sy;
+		uv[23].x = ex; uv[23].y = ey;
 
 		// Front triangles
 		tri[0] = 0;
@@ -136,36 +186,36 @@ public class VoxelPlaneScript : MonoBehaviour {
 		tri[11] = 7;
 
 		// Top triangles
-		tri[12] = 1;
-		tri[13] = 5;
-		tri[14] = 3;
-		tri[15] = 3;
-		tri[16] = 5;
-		tri[17] = 7;
+		tri[12] = 8;
+		tri[13] = 9;
+		tri[14] = 10;
+		tri[15] = 10;
+		tri[16] = 9;
+		tri[17] = 11;
 
 		// Bottom triangles
-		tri[18] = 4;
-		tri[19] = 0;
-		tri[20] = 6;
-		tri[21] = 6;
-		tri[22] = 0;
-		tri[23] = 2;
+		tri[18] = 13;
+		tri[19] = 12;
+		tri[20] = 15;
+		tri[21] = 15;
+		tri[22] = 12;
+		tri[23] = 14;
 
 		// Left triangles
-		tri[24] = 4;
-		tri[25] = 5;
-		tri[26] = 0;
-		tri[27] = 0;
-		tri[28] = 5;
-		tri[29] = 1;
+		tri[24] = 16;
+		tri[25] = 17;
+		tri[26] = 18;
+		tri[27] = 18;
+		tri[28] = 17;
+		tri[29] = 19;
 
 		// Right triangles
-		tri[30] = 2;
-		tri[31] = 3;
-		tri[32] = 6;
-		tri[33] = 6;
-		tri[34] = 3;
-		tri[35] = 7;
+		tri[30] = 22;
+		tri[31] = 23;
+		tri[32] = 21;
+		tri[33] = 21;
+		tri[34] = 23;
+		tri[35] = 20;
 
 		Mesh mesh = new Mesh();
 		mesh.vertices = v;
