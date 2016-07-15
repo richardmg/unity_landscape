1dacdda79486c34b3a4cd1faf722a9e512412999
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-07-15 10:36:55 +0200

    Add missing prefabs

---
+++ Assets/Resources/Editor/LandscapeConstructorEditor.cs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LandscapeConstructor))]
+public class LandscapeConstructorEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		LandscapeConstructor myScript = (LandscapeConstructor)target;
+		if(GUILayout.Button("Build landscape"))
+		{
+			myScript.rebuildLandscape();
+		}
+	}
+}
