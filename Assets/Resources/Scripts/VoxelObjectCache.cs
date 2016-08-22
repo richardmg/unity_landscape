f67db0d82b9429e1c4df6e19e7da6e8f1b028efa
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2016-08-22 16:09:38 +0200

    Add new prefabs

---
+++ Assets/Resources/Scripts/VoxelObjectCache.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using Lod = System.Int32;
+
+public class VoxelObjectCache {
+
+	static VoxelObjectCache s_instance;
+	Hashtable m_hashTable = new Hashtable();
+
+	private VoxelObjectCache()
+	{
+		s_instance = null;
+	}
+
+	static public VoxelObjectCache instance()
+	{
+		if (s_instance == null)
+			s_instance = new VoxelObjectCache();
+		return s_instance;
+	}
+
+	public Mesh getSharedMesh(string name, Lod lod)
+	{
+		string cacheId = name + (lod == VoxelObject.kLod0 ? "0" : "1");
+		Mesh mesh = (Mesh)m_hashTable[cacheId];
+
+		if (mesh == null) {
+			GameObject prefab = (GameObject)Resources.Load("Prefabs/" + name, typeof(GameObject));
+			if (prefab == null)
+				return null;
+
+			VoxelObject vo = prefab.GetComponent<VoxelObject>();
+			mesh = vo.createTopLevelMesh(lod);
+			m_hashTable[cacheId] = mesh;
+
+			if (m_hashTable.Count > 10) {
+				// Reminder for later...
+				MonoBehaviour.print("REMEMBER TO CLEAR CACHE FOR UNUSED VOXELOBJECTS! Cache size: " + m_hashTable.Count);
+			}
+		}
+
+		return mesh;
+	}
+
+	public void clearCache()
+	{
+		m_hashTable.Clear();
+	}
+}
