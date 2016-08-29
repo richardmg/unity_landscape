58dc1399080199bdf77a7d171fb7168d4e89fe0e
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2016-08-29 10:28:58 +0200

    Move perlin algorithm into landscape tile layer

--- Assets/Resources/Scripts/TileLayerTerrain.cs
+++ Assets/Resources/Scripts/TileLayerTerrain.cs
@@ -4,21 +4,37 @@ using System.Collections.Generic;
 
 public class TileLayerTerrain : MonoBehaviour, ITileTerrainLayer
 {
+	[Range (0, 700)]
+	public float oct0 = 200;
+	[Range (0, 200)]
+	public float oct1 = 10;
+	[Range (0, 20)]
+	public float oct3 = 1;
 	[Range (8, 512)]
 	public int groundResolution = 33;
 	[Range (0, 200)]
 	public int pixelError = 50;
 	public Texture2D terrainTexture;
+	public bool showInEditor = false;
 
 	GameObject[,] m_tileMatrix;
 	TerrainData m_terrainData;
 	public float[,] m_heightArray;
 
+	[HideInInspector]
+	public float noiseScaleOct0 = 0.003f;
+	[HideInInspector]
+	public float noiseScaleOct1 = 0.02f;
+	[HideInInspector]
+	public float noiseScaleOct2 = 0.1f;
+
 	public void OnValidate()
 	{
 		TileEngine engine = GetComponentInParent<TileEngine>();
-		if (engine)
-			engine.OnValidate();
+		if (showInEditor)
+			engine.updateAllTiles();
+		else
+			engine.removeAllTiles();
 	}
 
 	public void initTileLayer(TileEngine engine)
@@ -58,7 +74,7 @@ public class TileLayerTerrain : MonoBehaviour, ITileTerrainLayer
 
 			for (int x = 0; x < res; ++x) {
 				for (int z = 0; z < res; ++z) {
-					float height = LandscapeConstructor.getGroundHeight(desc.worldPos.x + (x * scale.x), desc.worldPos.z + (z * scale.z));
+					float height = getGroundHeight(desc.worldPos.x + (x * scale.x), desc.worldPos.z + (z * scale.z));
 					m_heightArray[z, x] = height / scale.y;
 				}
 			}
@@ -95,5 +111,13 @@ public class TileLayerTerrain : MonoBehaviour, ITileTerrainLayer
 	Terrain getTerrainSafe(Vector2 matrixPos)
 	{
 		return (int)matrixPos.x == -1 ? null : m_tileMatrix[(int)matrixPos.x, (int)matrixPos.y].GetComponent<Terrain>();
+	}
+
+	public float getGroundHeight(float x, float z)
+	{
+		float o0 = Mathf.PerlinNoise(x * noiseScaleOct0, z * noiseScaleOct0) * oct0;
+		float o1 = Mathf.PerlinNoise(x * noiseScaleOct1, z * noiseScaleOct1) * oct1;
+		float o2 = Mathf.PerlinNoise(x * noiseScaleOct2, z * noiseScaleOct2) * oct3;
+		return o0 + o1 + o2;
 	}
 }
