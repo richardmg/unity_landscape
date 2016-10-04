4ceca3c589078c82f8f7903ae019c3f1290be462
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-10-04 19:14:49 +0200

    Listen for mouse in color picker

---
+++ Assets/Resources/Scripts/ColorPicker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColorPicker : MonoBehaviour {
+
+	void Update ()
+	{
+		if (!Input.GetMouseButton(0))
+			return;
+
+		RawImage image = GetComponent<RawImage>();
+		Vector2 uv = UIManager.getMousePosOnImage(image);
+		if (!UIManager.isInside(uv))
+			return;
+
+		Texture2D texture = (Texture2D)image.texture;
+		int pixelX = (int)(uv.x * texture.width);
+		int pixelY = (int)(uv.y * texture.height);
+		Color color = texture.GetPixel(pixelX, pixelY);
+		print(color);
+	}
+}
