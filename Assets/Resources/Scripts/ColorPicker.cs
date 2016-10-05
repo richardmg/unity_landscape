63cc9fbff231b39e98ef4367db976040ab272796
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2016-10-05 16:30:04 +0200

    ColorPicker: click outside to select blank

--- Assets/Resources/Scripts/ColorPicker.cs
+++ Assets/Resources/Scripts/ColorPicker.cs
@@ -8,19 +8,21 @@ public class ColorPicker : MonoBehaviour {
 
 	void Update ()
 	{
-		if (!Input.GetMouseButtonDown(0))
+		if (!Root.instance.uiManager.grabMouse(this))
 			return;
 
 		RawImage image = GetComponent<RawImage>();
 		Vector2 uv = UIManager.getMousePosOnImage(image);
-		if (!UIManager.isInside(uv))
-			return;
 
-		Texture2D texture = (Texture2D)image.texture;
-		int pixelX = (int)(uv.x * texture.width);
-		int pixelY = (int)(uv.y * texture.height);
+		if (!UIManager.isInside(uv)) {
+			selectedColor = Color.clear;
+		} else {
+			Texture2D texture = (Texture2D)image.texture;
+			int pixelX = (int)(uv.x * texture.width);
+			int pixelY = (int)(uv.y * texture.height);
+			selectedColor = texture.GetPixel(pixelX, pixelY);
+		}
 
-		selectedColor = texture.GetPixel(pixelX, pixelY);
 		Root.instance.uiManager.pop(true);
 	}
 }
