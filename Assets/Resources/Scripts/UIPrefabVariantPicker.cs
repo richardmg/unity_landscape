6290c098319b503c3b62362b20638182d6373355
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2016-10-13 11:32:14 +0200

    Let player hold a custom prefabvariant on startup

--- Assets/Resources/Scripts/UIPrefabVariantPicker.cs
+++ Assets/Resources/Scripts/UIPrefabVariantPicker.cs
@@ -5,9 +5,6 @@ public class UIPrefabVariantPicker : MonoBehaviour {
 
 	public void onNewTreeButtonClicked()
 	{
-		// Redirect button click here
-//		PrefabVariant variant = new PrefabVariant("TreeFilled");
-//		Root.instance.player.currentPrefabVariant = variant;
-//		Root.instance.uiManager.push(Root.instance.uiManager.paintEditorGO, (bool accepted) => {});
+		Root.instance.uiManager.push(Root.instance.uiManager.paintEditorGO, (bool accepted) => {});
 	}
 }
