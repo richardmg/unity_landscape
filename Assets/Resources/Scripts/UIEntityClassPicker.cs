72439ba9ea85d0be18fa7e019cf87d61740968e1
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-10-15 21:09:36 +0200

    Add render to texture for picker

--- Assets/Resources/Scripts/UIEntityClassPicker.cs
+++ Assets/Resources/Scripts/UIEntityClassPicker.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 
 public class UIEntityClassPicker : MonoBehaviour {
 
+	public GameObject uiEntityPickerCamera;
+
 	EntityInstance instance;
 
 	void OnEnable()
 	{
 		List<EntityClass> entityClasses = Root.instance.entityManager.allEntityClasses;
 		for (int i = 0; i < entityClasses.Count; ++i) {
-			instance = entityClasses[i].createInstance(Root.instance.playerGO.transform, "Entity in picker");
+			instance = entityClasses[i].createInstance(uiEntityPickerCamera.transform);
+			instance.gameObject.layer = LayerMask.NameToLayer("UIEntityPickerLayer");
 			instance.makeStandalone();
-			instance.transform.localPosition = new Vector3(0, 0, 10);
-			instance.gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-			instance.gameObject.GetComponent<Renderer>().sortingLayerName = "MyUI";
+			instance.transform.localPosition = new Vector3(0, 0, 1);
+			float scale = 0.01f;
+			instance.gameObject.transform.localScale = new Vector3(scale, scale, scale);
 		}
 	}
 
