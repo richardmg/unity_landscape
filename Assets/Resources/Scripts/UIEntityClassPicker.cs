a84757411c18b57c036f8e62be0aa2df2fbc74df
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2016-10-16 11:31:32 +0200

    Factor out takeSnapshot in separate function

--- Assets/Resources/Scripts/UIEntityClassPicker.cs
+++ Assets/Resources/Scripts/UIEntityClassPicker.cs
@@ -1,36 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class UIEntityClassPicker : MonoBehaviour {
 
-	public GameObject uiEntityPickerCamera;
-
-	List<EntityInstance> entityInstanceList = new List<EntityInstance>();
+	public GameObject uiEntityPickerCameraGO;
+	public GameObject rawImageGO;
 
 	void OnEnable()
 	{
 		List<EntityClass> entityClasses = Root.instance.entityManager.allEntityClasses;
-		for (int i = 0; i < entityClasses.Count; ++i)
-			createEntityInstance(entityClasses[i]);
+		for (int i = 0; i < entityClasses.Count; ++i) {
+			Texture2D snapshot = takeSnapshot(entityClasses[i]);
+			rawImageGO.GetComponent<RawImage>().texture = snapshot;
+		}
 	}
 
 	void OnDisable()
 	{
-		foreach (EntityInstance i in entityInstanceList)
-			GameObject.Destroy(i.gameObject);
-		entityInstanceList = new List<EntityInstance>();
 	}
 
-	void createEntityInstance(EntityClass entityClass)
+	Texture2D takeSnapshot(EntityClass entityClass)
 	{
-		EntityInstance instance = entityClass.createInstance(uiEntityPickerCamera.transform, entityClass.name);
-		instance.gameObject.layer = LayerMask.NameToLayer("UIEntityPickerLayer");
+		EntityInstance instance = entityClass.createInstance(uiEntityPickerCameraGO.transform, entityClass.name);
 		instance.makeStandalone();
+		instance.gameObject.layer = LayerMask.NameToLayer("UIEntityPickerLayer");
 		instance.transform.localPosition = new Vector3(0, 0, 1);
 		float scale = 0.01f;
 		instance.gameObject.transform.localScale = new Vector3(scale, scale, scale);
-		entityInstanceList.Add(instance);
+
+		Camera camera = uiEntityPickerCameraGO.GetComponent<Camera>();
+        RenderTexture currentRT = RenderTexture.active;
+		RenderTexture.active = camera.targetTexture;
+		camera.Render();
+
+		Texture2D snapshot = new Texture2D(camera.targetTexture.width, camera.targetTexture.height);
+		snapshot.ReadPixels(new Rect(0, 0, camera.targetTexture.width, camera.targetTexture.height), 0, 0);
+		snapshot.Apply();
+
+        RenderTexture.active = currentRT;
+		instance.gameObject.SetActive(false);
+		GameObject.Destroy(instance);
+		return snapshot;
 	}
 
 	public void onNewTreeButtonClicked()
