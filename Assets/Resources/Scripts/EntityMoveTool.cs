df7ffedc5c6e5e1b137e8317f7f565004b290233
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2017-01-09 19:29:34 +0100

    MoveTool: move in arrow direction

--- Assets/Resources/Scripts/EntityMoveTool.cs
+++ Assets/Resources/Scripts/EntityMoveTool.cs
@@ -12,32 +12,59 @@ public class EntityMoveTool : MonoBehaviour
 
 	public void onLeftButtonClicked()
 	{
-		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
-			desc.worldPos.x -= Root.instance.entityBaseScale.x;
-			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
-		}
+		handleLeftOrRightButtonClicked(1);
 	}
 
 	public void onRightButtonClicked()
 	{
+		handleLeftOrRightButtonClicked(-1);
+	}
+
+	public void onUpButtonClicked()
+	{
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
-			desc.worldPos.x += Root.instance.entityBaseScale.x;
+			desc.worldPos.y += Root.instance.entityBaseScale.y;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
 	}
 
-	public void onForwardButtonClicked()
+	public void onDownButtonClicked()
 	{
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
-			desc.worldPos.z += Root.instance.entityBaseScale.x;
+			desc.worldPos.y -= Root.instance.entityBaseScale.y;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
 	}
 
-	public void onBackwardButtonClicked()
+	public void handleLeftOrRightButtonClicked(int leftButton)
+	{
+		EntityInstanceDescription mainDesc = Root.instance.player.selectedEntityInstances[0];
+		Vector3 targetPos = mainDesc.worldPos;
+		Vector3 playerPos = Root.instance.playerGO.transform.position;
+		Vector3 relativePos = targetPos - playerPos;
+
+		bool playerInFront = relativePos.z > 0;
+		bool playerOnRight = relativePos.x > 0;
+		bool moreFrontThanSide = Mathf.Abs(relativePos.z) > Mathf.Abs(relativePos.x);
+
+		if (moreFrontThanSide)
+			moveAlongX(playerInFront ? -leftButton : leftButton);
+		else
+			moveAlongZ(playerOnRight ? leftButton : -leftButton);
+	}
+
+	void moveAlongX(int direction)
 	{
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
-			desc.worldPos.z -= Root.instance.entityBaseScale.x;
+			desc.worldPos.x += Root.instance.entityBaseScale.x * direction;
+			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
+		}
+	}
+
+	void moveAlongZ(int direction)
+	{
+		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
+			desc.worldPos.z += Root.instance.entityBaseScale.x * direction;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
 	}
