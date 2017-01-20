2eb61fe65198283afc6724583650ff2ad11a153d
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2017-01-20 11:01:04 +0100

    Align also after rotate

--- Assets/Resources/Scripts/EntityRotateTool.cs
+++ Assets/Resources/Scripts/EntityRotateTool.cs
@@ -147,6 +147,8 @@ public class EntityRotateTool : MonoBehaviour
 
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
 			desc.instance.transform.Rotate(alignedDistance, 0, 0, Space.Self);
+			Root.instance.worldScaleManager.align(desc.instance.transform);
+			desc.worldPos = desc.instance.transform.position;
 			desc.rotation = desc.instance.transform.rotation;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
@@ -161,6 +163,8 @@ public class EntityRotateTool : MonoBehaviour
 
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
 			desc.instance.transform.Rotate(0, alignedDistance, 0, Space.Self);
+			Root.instance.worldScaleManager.align(desc.instance.transform);
+			desc.worldPos = desc.instance.transform.position;
 			desc.rotation = desc.instance.transform.rotation;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
@@ -175,6 +179,8 @@ public class EntityRotateTool : MonoBehaviour
 
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
 			desc.instance.transform.Rotate(0, 0, alignedDistance, Space.Self);
+			Root.instance.worldScaleManager.align(desc.instance.transform);
+			desc.worldPos = desc.instance.transform.position;
 			desc.rotation = desc.instance.transform.rotation;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
