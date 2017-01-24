9db258ab008a64e8495c08d2d17f805f7d115154
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2017-01-24 23:47:18 +0100

    Switch on align

--- Assets/Resources/Scripts/AlignmentManager.cs
+++ Assets/Resources/Scripts/AlignmentManager.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 public class AlignmentManager : MonoBehaviour
 {
 	public Vector3 voxelSize = new Vector3(0.2f, 0.2f, 0.2f);
-	public float rotationStep = 22.5f;
+	public float rotationStep = 5f;
 
 	public float align(float v, float unit)
 	{
-		return Mathf.Floor(v / unit) * unit;
+		return Mathf.Round(v / unit) * unit;
 	}
 
 	public Vector3 align(Vector3 v)
@@ -29,10 +29,13 @@ public class AlignmentManager : MonoBehaviour
 	public void align(List<EntityInstanceDescription> selection)
 	{
 		foreach (EntityInstanceDescription desc in selection) {
-//			align(desc.instance.transform);
-//			desc.worldPos = desc.instance.transform.position;
-//			desc.rotation = desc.instance.transform.rotation;
-//			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
+			desc.voxelRotation.x = align(desc.voxelRotation.x, rotationStep);
+			desc.voxelRotation.y = align(desc.voxelRotation.y, rotationStep);
+			desc.voxelRotation.z = align(desc.voxelRotation.z, rotationStep);
+			desc.instance.transform.setVoxelRotation(desc.voxelRotation);
+			align(desc.instance.transform);
+			desc.worldPos = desc.instance.transform.position;
+			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
 	}
 
