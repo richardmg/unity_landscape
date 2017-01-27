b9f09fe5d1f800e51b52887bec76a0adbda1b267
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2017-01-27 23:48:10 +0100

    RotationTool finally works

--- Assets/Resources/Scripts/EntityRotateTool.cs
+++ Assets/Resources/Scripts/EntityRotateTool.cs
@@ -14,10 +14,7 @@ public class EntityRotateTool : MonoBehaviour, IEntityInstanceSelectionListener
 		Root.instance.player.setWalkSpeed(1);
 		onSelectionChanged(Root.instance.player.selectedEntityInstances, Root.instance.player.selectedEntityInstances);
 		Root.instance.notificationManager.addEntitySelectionListener(this);
-
-		Transform firstTransform = Root.instance.player.selectedEntityInstances[0].instance.transform;
-		m_pushDirectionZ = (int)Root.instance.playerGO.transform.getVoxelPushDirection(firstTransform, false, false, true, Space.Self).z;
-		m_tippedBack = Vector3.Dot(Vector3.forward, firstTransform.up) < 0 ? 1 : -1;
+		updatePushDirection();
 	}
 
 	public void OnDisable()
@@ -38,10 +35,16 @@ public class EntityRotateTool : MonoBehaviour, IEntityInstanceSelectionListener
 
 		if (Root.instance.entityToolManager.playerIdle()) {
 			Root.instance.alignmentManager.align(Root.instance.player.selectedEntityInstances);
-			Transform firstTransform = Root.instance.player.selectedEntityInstances[0].instance.transform;
-			m_pushDirectionZ = (int)Root.instance.playerGO.transform.getVoxelPushDirection(firstTransform, false, false, true, Space.Self).z;
-			m_tippedBack = Vector3.Dot(Vector3.forward, firstTransform.up) < 0 ? 1 : -1;
+			updatePushDirection();
 		}
+	}
+
+	void updatePushDirection()
+	{
+		Transform playerTransform = Root.instance.playerGO.transform;
+		Transform firstTransform = Root.instance.player.selectedEntityInstances[0].instance.transform;
+		m_pushDirectionZ = (int)Root.instance.playerGO.transform.getVoxelPushDirection(firstTransform, false, false, true, Space.Self).z;
+		m_tippedBack = Vector3.Dot(playerTransform.forward, firstTransform.up) < 0 ? 1 : -1;
 	}
 
 	void updateRotate()
@@ -54,8 +57,7 @@ public class EntityRotateTool : MonoBehaviour, IEntityInstanceSelectionListener
 		// Inform the app about the position update of the selected objects
 		foreach (EntityInstanceDescription desc in Root.instance.player.selectedEntityInstances) {
 			desc.voxelRotation.x += (m_pushDirectionZ > 0 ? playerMovement.y : -playerMovement.y) * 40;
-			desc.voxelRotation.y += (m_pushDirectionZ > 0 ? playerMovement.x : -playerMovement.x)
-				* ((straightUp && m_pushDirectionZ > 0) ? 1 : m_tippedBack) * 40;
+			desc.voxelRotation.y += (straightUp ? playerMovement.x : playerMovement.x * m_tippedBack) * 40;
 			Root.instance.notificationManager.notifyEntityInstanceDescriptionChanged(desc);
 		}
 	}
