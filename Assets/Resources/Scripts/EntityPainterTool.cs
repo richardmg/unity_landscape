1aa8a7cfbd2371785990885b2187bf3c62e507ea
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2017-02-10 11:36:12 +0100

    Add EntityPainterTool

---
+++ Assets/Resources/Scripts/EntityPainterTool.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class EntityPainterTool : MonoBehaviour
+{
+	public void OnEnable()
+	{
+		Root.instance.uiManager.grabMouse(this);
+	}
+
+	void Update()
+	{
+		if (!Input.GetMouseButtonDown(0))
+			return;
+
+		Root.instance.entityToolManager.selectionTool.selectSingleObjectUnderPointer();
+		if (Root.instance.player.selectedEntityInstances.Count == 0)
+			return;	
+
+		Debug.Log("Paint: " + Root.instance.player.selectedEntityInstances[0].instance);
+	}
+}
