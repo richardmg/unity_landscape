71b40f336eda45d1eb1de7866ac168d4d7e2546c
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2017-02-12 20:34:52 +0100

    Add UIToolMenu

---
+++ Assets/Resources/Scripts/UIToolMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class UIToolMenu : MonoBehaviour 
+{
+	public void onAddBlankSheetButtonClicked()
+	{
+		Root.instance.player.entityClassInUse = Root.instance.entityClassManager.getEntity(0);
+		Root.instance.uiManager.setMenuVisible(false);
+	}
+}
